74cb4b13483b5fc8ba9dd73d532b2459e6d1b969
Riccardo Stefani <riccardost02@gmail.com>
2024-03-10 11:10:19 +0100

    Java

--- Hello_World/Hello_world.java
+++ Hello_World/Hello_world.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+public class Hello_world {
     public static void main(String[] args) {
-        System.out.println("Hello, World! I'm a Java program");
+        System.out.println("Hello, World! I'm a Java program. I want to be modified in local");
     }
 }
