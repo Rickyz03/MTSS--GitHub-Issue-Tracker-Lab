86f23afa560cdf6d7c758bed54842665550abd92
Riccardo Stefani <riccardost02@gmail.com>
2024-03-10 12:36:19 +0100

    Revert "E ora"
    This reverts commit 950eefb1fca537d088de11c319d382fc1b02814b.

--- Hello_World/Hello_world.cpp
+++ Hello_World/Hello_world.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 
 int main(){
-    std::cout<<"Hello World from Ricky and Leo! That's amazing! Questa è una modifica in locale!!!! Test Staging Area NO"<<std::endl;
+    std::cout<<"Hello World from Ricky and Leo! That's amazing! Questa è una modifica in locale!!!!"<<std::endl;
 }
